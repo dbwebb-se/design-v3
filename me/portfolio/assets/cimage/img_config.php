b895c0956bc8c8e8ad9d387d2c26502945abbedc
Niklas Andersson <nhdandersson@gmail.com>
2020-08-26 14:06:11 +0200

    Now ignores me folder

--- me/portfolio/assets/cimage/img_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    "mode"         => "development",
-    "image_path"   =>  __DIR__ . "/../../assets/img/",
-    "cache_path"   =>  __DIR__ . "/../../cache/cimage/",
-    "autoloader"   =>  __DIR__ . "/../../vendor/autoload.php",
-];
--- me/portfolio/content/_meta.md
+++
@@ -1,8 +0,0 @@
----
-Logo: image/leaf_256x256.png
-Tagline: My portfolio.
-Social:
-    - title: Link till sidans github repo.
-      url: https://github.com/dbwebb-se/design-v3
-      icon: github
----
