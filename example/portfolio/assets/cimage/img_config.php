0cfb0b022c09a6f5c8d86a7de33c1594932ea5c2
Martin Borg <Martin.d@live.se>
2020-08-26 09:44:17 +0200

    changed cimage config

--- example/portfolio/assets/cimage/img_config.php
+++ example/portfolio/assets/cimage/img_config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     "mode"         => "development",
-    "image_path"   =>  __DIR__ . "/../img/",
+    "image_path"   =>  __DIR__ . "/../../assets/img/",
     "cache_path"   =>  __DIR__ . "/../../cache/cimage/",
     "autoloader"   =>  __DIR__ . "/../../vendor/autoload.php",
 ];
