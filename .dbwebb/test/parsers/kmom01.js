99a2dd64762df901dbe330b5087fd49b3d4055c1
Emil Folino <emilfolino@gmail.com>
2021-10-13 12:22:08 +0200

    More tests for test script

--- .dbwebb/test/parsers/kmom01.js
+++ .dbwebb/test/parsers/kmom01.js
@@ -5,8 +5,15 @@ const url = process.argv[2];
 
 (async () => {
     try {
+        const response = await got(url + "/github.txt");
+        console.log("txt", response.body);
+    } catch (error) {
+        console.log(error.response.body);
+    }
+
+    try {
         const response = await got(url);
-        console.log(response.body);
+        console.log("html", response.body);
     } catch (error) {
         console.log(error.response.body);
     }
