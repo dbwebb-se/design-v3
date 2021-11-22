ba7ab3a4d13b471c4eaae36bb06b98098cd051a7
Emil Folino <emilfolino@gmail.com>
2021-11-22 20:35:50 +0100

    Added lighthouse node module and tests for kmom06

--- .dbwebb/test/parsers/kmom06.js
+++ .dbwebb/test/parsers/kmom06.js
@@ -1,5 +1,7 @@
 const HTMLParser = require('node-html-parser');
 const got = require('got');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
 
 const tags = require("./tags.js");
 
@@ -11,33 +13,60 @@ const url = process.argv[2];
     console.log("  Performing tags checks   ");
     console.log("===================================");
     const versionTag = 6;
+
+    let tagCheck = false;
     if (await tags.checkTag(url, versionTag)) {
         console.log(`\u{1F973}\tCorrect ${versionTag}.*.* tags found in GitHub repo.\n`);
+        tagCheck = true;
     } else {
         console.log(`\u{1F928}\tTags were missing for ${versionTag}.*.*, remember to tag repo.\n`);
     }
 
-    // Fetching and parsing portfolio
-    let portfolioChecks = false;
-
     try {
-        const response = await got(url);
-        const root = HTMLParser.parse(response.body);
+        console.log(`===================================
+   Performing accesibility checks
+   (Might take a while)
+===================================`);
 
-        // Create messages array
-        let messages = [];
+        let a11yCheck = false;
 
+        const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+        const options = { output: 'json', onlyCategories: ['accessibility'], port: chrome.port, quiet: true };
+        const indexResult = await lighthouse(url, options);
+
+        let indexScore = indexResult.lhr.categories.accessibility.score;
+
+        console.log("\tAccessibility score of index: " + (indexScore * 100));
+
+        const galleryResult = await lighthouse(url + "/gallery", options);
+
+        let galleryScore = galleryResult.lhr.categories.accessibility.score;
+
+        console.log("\tAccessibility score of gallery: " + (galleryScore * 100));
+
+        const techResult = await lighthouse(url + "/technology", options);
+
+        let techScore = techResult.lhr.categories.accessibility.score;
+
+        console.log("\tAccessibility score of technology: " + (techScore * 100));
+
+        if (indexScore === 1.0 &&
+                galleryScore === 1.0 &&
+                techScore === 1.0) {
+            console.log("\n\u{1F973}\tAccessibility score equal to 100.");
+            a11yCheck = true;
+        } else {
+            console.log("\n\u{1F928}\tAccessibility score less than 100.");
+        }
+
+        await chrome.kill();
+
+        console.log("\n===================================");
+        console.log("  Summary    ");
         console.log("===================================");
-        console.log("  Performing portfolio checks   ");
-        console.log("===================================");
-
-
-
-        // Output all messages
-        console.log(messages.join("\n"));
 
         // Exit with correct status
-        if (portfolioChecks) {
+        if (tagCheck && a11yCheck) {
             console.log("\n\u{1F973}\tAll checks passed.");
             process.exit(0);
         } else {
@@ -45,8 +74,8 @@ const url = process.argv[2];
             process.exit(1);
         }
     } catch (error) {
-        console.log(error.message);
-        console.log("\u{1F928}No response from studentserver");
+        console.error(error);
+        console.log("\u{1F928}\tCould not find page.");
         process.exit(1);
     }
 })();
